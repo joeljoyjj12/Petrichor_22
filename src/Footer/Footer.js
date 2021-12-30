dcfc24f1c58fccc777368a2db1709b5ec8cdaf0b
Jerry John Thomas <jerryjohnthomastvm@gmail.com>
2021-12-31 01:47:14 +0530

    cult events 3 up

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,71 +1,15 @@
 import React, { useEffect,useState } from 'react'
 import Contact_content from './Contact_content'
 import "./Footer.css"
+import {gsap} from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
 
 function Footer() {
-    let ht="200px";
-    let [hfind,setHfind]=useState("");
-
-    let [shouldIgocrazy, setShouldIgocrazy]=useState(true);
-    useEffect(()=>{
-
-        let e=document.querySelector(".bottom_contact");
-        let oh=e.offsetHeight;
-        let m1=e.style.marginTop;
-        let m2=e.style.marginBottom;
-        let ans =oh+m1+m2;
-
-        
-        
-        console.log("ans is "+ans);
-        let ans2=ans.toString();
-        // ans2+="px !important";
-        ans2+="px";
-        let d1=document.querySelector("#jery_footer_id");
-        let d2=document.querySelector("#jerry_footer_crazy_support_id");
-        d2.style.height=ans2;
-        d1.style.height=ans2;
-        console.log(d2.style.height+ " our ans2 is "+ans2+ " our ans is "+ans);
-
-        // let w = window.innerWidth;
-        // let h = window.innerHeight;
-        // if(w>800)
-        //     setShouldIgocrazy(true);
-        // else
-        //     setShouldIgocrazy(false);
-
-        window.addEventListener('scroll',(e)=>{
-            let element=document.querySelector("#jery_footer_id");
-            let cr=element.getBoundingClientRect();
-
-
-        })
-
-        document.addEventListener("click",(e)=>{
-            console.log(e.target.classList);
-            console.log(e.target.classname);
-        });
-    },[]);
-    
     return (
-            <>
-                       
-          
-                       <div id="jerry_footer_crazy_support_id" className="jerry_footer_crazy_support" style={shouldIgocrazy?{background:"rgba(0,0,255,0.4)",height:"543px",width:"100vw",zIndex:"20"}:null}></div>
-            
-            <div id="jery_footer_id" classname="jerry_footer_crazy" 
-                // style={shouldIgocrazy?{position:"fixed",background:"transparent",width:"100vw",bottom:"0px",zIndex:"-1",height:hfind}:{}}
-                style={shouldIgocrazy?{position:"fixed",background:"transparent",width:"100vw",bottom:"0px",zIndex:"-1",height:hfind}:{}}
-                    >
-                    <Contact_content />
-            </div>
-
-
-
-
-      
-        </>
+            <Contact_content />
     )
 }
 
 export default Footer
+
+
--- src/data/Cultural_Events_data.js
+++ src/data/Cultural_Events_data.js
@@ -1,245 +1,191 @@
 const Cult_events=[
     {
-        name : "singing comp",
+        name : "soliloquy",
         // jerry check if it is yyyyy-mm-dd or reverse
         start_date : "12-02-2021",
         end_date : "14-02-2022",
         // date to compete link
         registeration_link : "https://www.google.com/",
         // the pic should be locally uploaded
-        picture: "/media/cult_event_pic/dance1.jpg",
+        picture: "/media/Event_Cultural/dance1.jpg",
         organiser:[
             {
-                name : "Antony Jerald",
-                pic :"/media/organisers/antony.png",
-                email :"anton@gmail.com",
+                name : "Harsh Kanani",
+                pic :"/media/organisers/Harsh_Kanani.jpg",
+                email :"9499725866",
             },
             {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
+                name : "Megha",
+                pic :"/media/organisers/Megha.jpg",
+                email :"9188184292",
             },
-            {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
-            }
         ],
         description_title : "Title",
         // first element will be bolded, I think u can only have 2 paras (map)
         description: ["StudioSeven is Maltas leading A/V and production hub. In 2019, StudioSeven celebrated their 40 year anniversary and contacted Bison Studio in order to help them create an online presence that was in line with their new vision. A website that truly represents what they’re all about", 
         "From the start of this project, we knew that StudioSeven wanted to stand out. They were not looking for a template approach that would look the same as their competitors. We instantly knew that this was the perfect project for us."
         ],
-        hover_description : "this is a great competition",
+        hover_description : "Monologue Competition",
        
         // have each rule as a element of an array
-        rule_book_rules :[
-                "You should know how to sing do ra mi fa so la ti do",
-                "You shouldnt suck ",
-                "No lip syncing",
-                "no malpractises will be entertained ok, absolute 0 bullying policy, you are supposed to sing this alone "
-        ] ,
+        rule_book_rules :["The language Should be English/Hindi.",
+        "It should not exceed more than 5 minutes.",
+        "Exceeding the time limit will cost into negative marking.",
+        "A rough script and a little intro should be provided before the deadline.",
+        "Video/Audio must be clear. No compromises will be done in terms of technical issues.",
+        "Appropriate Background, BGM music, Use of props will attract bonus points.",
+        "The decision of the judges will be final.",
+        "Plagiarism will not be tolerated."]
+        ,
 
         //recommended always show true,
         //instructure in each array row, the first 
         structure_show:true,
         structure:[
-            ["Prlimilnary Round","each team will have 2 3 days to prep","failure to show up is dump"],
-            ["R2","you win man","you guys r goat"],
+            ["General","Entries will be accepted through a Google Form along with essential information like Name, Phn_no., E-mail, etc."]
         ],
         
         
         
         // reccommended make is true and write down the rest
         judging_criteria_show : true,
-        judging_criteria : ["quality of sound produced", " your own twist to the original song"],
+        judging_criteria : ["Acting Skills: 40",
+        "Cosplay: 15",
+        "Writing: 15",
+        "Originality: 10",
+        "Message: 10",
+        "Max Bonus Points: 10"],
 
 
 
-        prizes_general_text : "We have a exciting prized waiting for you",
-        prizes_in_ascending_order: ["Rs. 10,000 ", "PS 5"],
+        prizes_general_text : "We have exciting prizes awaiting you",
+        prizes_in_ascending_order: ["Rs. 3,000 ", "Rs. 1500","Rs. 750"],
     },
     {
-        name : "singing comp",
+        name : "Relive it",
         // jerry check if it is yyyyy-mm-dd or reverse
         start_date : "12-02-2021",
         end_date : "14-02-2022",
         // date to compete link
         registeration_link : "https://www.google.com/",
         // the pic should be locally uploaded
-        picture: "/media/cult_event_pic/car.jpg",
+        picture: "/media/Event_Cultural/dance1.jpg",
         organiser:[
             {
-                name : "Antony Jerald",
-                pic :"/media/organisers/antony.png",
-                email :"anton@gmail.com",
+                name : "Harsh Kanani",
+                pic :"/media/organisers/Harsh_Kanani.jpg",
+                email :"9499725866",
             },
             {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
+                name : "Megha",
+                pic :"/media/organisers/Megha.jpg",
+                email :"9188184292",
             },
-            {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
-            }
         ],
         description_title : "Title",
         // first element will be bolded, I think u can only have 2 paras (map)
         description: ["StudioSeven is Maltas leading A/V and production hub. In 2019, StudioSeven celebrated their 40 year anniversary and contacted Bison Studio in order to help them create an online presence that was in line with their new vision. A website that truly represents what they’re all about", 
         "From the start of this project, we knew that StudioSeven wanted to stand out. They were not looking for a template approach that would look the same as their competitors. We instantly knew that this was the perfect project for us."
         ],
-        hover_description : "this is a great competition",
+        hover_description : "Reenact a movie",
        
         // have each rule as a element of an array
-        rule_book_rules :[
-                "You should know how to sing do ra mi fa so la ti do",
-                "You shouldnt suck ",
-                "No lip syncing",
-                "no malpractises will be entertained ok, absolute 0 bullying policy, you are supposed to sing this alone "
-        ] ,
+        rule_book_rules :["The language Should be English/Hindi.",
+        "It should not exceed more than 15 minutes.",
+        "Exceeding the time limit will cost into negative marking.",
+        "A little intro/Youtube link to the original scene should be provided before the deadline.",
+        "Video/Audio must be clear. No compromises will be done in terms of technical issues.",
+        "Appropriate Background, BGM music, Use of props will attract bonus points.",
+        "The decision of the judges will be final."]
+        ,
 
         //recommended always show true,
         //instructure in each array row, the first 
         structure_show:true,
         structure:[
-            ["Prlimilnary Round","each team will have 2 3 days to prep","failure to show up is dump"],
-            ["R2","you win man","you guys r goat"],
+            ["General","Entries will be accepted through a Google Form along with essential information like Name, Phn_no., E-mail, etc."]
         ],
         
         
         
         // reccommended make is true and write down the rest
         judging_criteria_show : true,
-        judging_criteria : ["quality of sound produced", " your own twist to the original song"],
+        judging_criteria : ["Acting Skills: 40",
+        "Cosplay: 20",
+        "Scene selection: 15",
+        "Message: 10",
+        "Max Bonus Points: 10"],
 
 
 
-        prizes_general_text : "We have a exciting prized waiting for you",
-        prizes_in_ascending_order: ["Rs. 10,000 ", "PS 5"],
+        prizes_general_text : "We have exciting prizes awaiting you",
+        prizes_in_ascending_order: ["Rs. 3,000 ", "Rs. 1500","Rs. 750"],
     },
     {
-        name : "singing comp",
+        name : "BGMI",
         // jerry check if it is yyyyy-mm-dd or reverse
         start_date : "12-02-2021",
         end_date : "14-02-2022",
         // date to compete link
         registeration_link : "https://www.google.com/",
         // the pic should be locally uploaded
-        picture: "/media/cult_event_pic/dance1.jpg",
+        picture: "/media/Event_Cultural/dance1.jpg",
         organiser:[
             {
-                name : "Antony Jerald",
-                pic :"/media/organisers/antony.png",
-                email :"anton@gmail.com",
+                name : "Amar Hamdan",
+                pic :"/media/organisers/Amar_Hamdan.jpg",
+                email :"8606665885",
             },
             {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
+                name : "Ashwin Kumar",
+                pic :"/media/organisers/Ashwin_Kumar.jpg",
+                email :"8240753402",
             },
-            {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
-            }
         ],
-        description_title : "Title",
         // first element will be bolded, I think u can only have 2 paras (map)
         description: ["StudioSeven is Maltas leading A/V and production hub. In 2019, StudioSeven celebrated their 40 year anniversary and contacted Bison Studio in order to help them create an online presence that was in line with their new vision. A website that truly represents what they’re all about", 
         "From the start of this project, we knew that StudioSeven wanted to stand out. They were not looking for a template approach that would look the same as their competitors. We instantly knew that this was the perfect project for us."
         ],
-        hover_description : "this is a great competition",
+        hover_description : "E Games",
        
         // have each rule as a element of an array
-        rule_book_rules :[
-                "You should know how to sing do ra mi fa so la ti do",
-                "You shouldnt suck ",
-                "No lip syncing",
-                "no malpractises will be entertained ok, absolute 0 bullying policy, you are supposed to sing this alone "
-        ] ,
+        rule_book_rules :["Players registering for the tournament must have completed minimum age of 16 years. If a player is over the age of 16 but under 18, he/she needs to provide a consent form from their parents or guardians.",
+        "Ranking Requirements: All players on a Team’s roster must have held a ranking of level 15 in the game and above Platinum V Tier, at the time of registration.",
+        "Single Team Exclusivity: A Team Member or the captain is allowed to compete for the one Team that they have an agreement with. A Team Member or coach will not be allowed to compete for more than one team.",
+        "Each Team must maintain, during the tournament exactly four players which is the minimum and maximum number of players allowed.",
+        "Only the registered players can play in the tournament. (If any other player is caught playing, then the team will be disqualified.)",
+        "Each team must select their captain who shall be responsible for communications with the officials of the tournament. A team cannot change the team captain during the tournament without prior approval from the tournament officials.",
+        "Players must not use an emulator to play on a PC or other device that is not a handheld device."],
 
         //recommended always show true,
         //instructure in each array row, the first 
         structure_show:true,
         structure:[
-            ["Prlimilnary Round","each team will have 2 3 days to prep","failure to show up is dump"],
-            ["R2","you win man","you guys r goat"],
-        ],
-        
-        
+            [
+                "Match structure",
+                "Game mode: Battle Royale (25 teams)", 
+                "There will be 3 matches on (Erangle, Miramar, Sanhok)",
+                "1 kill = 1 point", 
+            ],
+            ["Position and Points",
+                "15 points",
+            "12 points",
+            "10 points",
+            "8 points",
+            "6 points",
+            "4 points",
+            "2 points",
+            "1 points",
+            ]
+            ],
         
         // reccommended make is true and write down the rest
-        judging_criteria_show : true,
-        judging_criteria : ["quality of sound produced", " your own twist to the original song"],
+        judging_criteria_show : false,
 
-
-
-        prizes_general_text : "We have a exciting prized waiting for you",
-        prizes_in_ascending_order: ["Rs. 10,000 ", "PS 5"],
+        prizes_general_text : "We have exciting prizes awaiting you",
+        prizes_in_ascending_order: ["Rs. 2,000 ", "Rs. 1750","Rs. 1250"],
     },
-    {
-        name : "singing comp",
-        // jerry check if it is yyyyy-mm-dd or reverse
-        start_date : "12-02-2021",
-        end_date : "14-02-2022",
-        // date to compete link
-        registeration_link : "https://www.google.com/",
-        // the pic should be locally uploaded
-        picture: "/media/cult_event_pic/car.jpg",
-        organiser:[
-            {
-                name : "Antony Jerald",
-                pic :"/media/organisers/antony.png",
-                email :"anton@gmail.com",
-            },
-            {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
-            },
-            {
-                name : "Abraham",
-                pic :"/media/organisers/person1.jpg",
-                email :"abrhm@gmail.com",
-            }
-        ],
-        description_title : "Title",
-        // first element will be bolded, I think u can only have 2 paras (map)
-        description: ["StudioSeven is Maltas leading A/V and production hub. In 2019, StudioSeven celebrated their 40 year anniversary and contacted Bison Studio in order to help them create an online presence that was in line with their new vision. A website that truly represents what they’re all about", 
-        "From the start of this project, we knew that StudioSeven wanted to stand out. They were not looking for a template approach that would look the same as their competitors. We instantly knew that this was the perfect project for us."
-        ],
-        hover_description : "this is a great competition",
-       
-        // have each rule as a element of an array
-        rule_book_rules :[
-                "You should know how to sing do ra mi fa so la ti do",
-                "You shouldnt suck ",
-                "No lip syncing",
-                "no malpractises will be entertained ok, absolute 0 bullying policy, you are supposed to sing this alone "
-        ] ,
-
-        //recommended always show true,
-        //instructure in each array row, the first 
-        structure_show:true,
-        structure:[
-            ["Prlimilnary Round","each team will have 2 3 days to prep","failure to show up is dump"],
-            ["R2","you win man","you guys r goat"],
-        ],
-        
-        
-        
-        // reccommended make is true and write down the rest
-        judging_criteria_show : true,
-        judging_criteria : ["quality of sound produced", " your own twist to the original song"],
-
-
-
-        prizes_general_text : "We have a exciting prized waiting for you",
-        prizes_in_ascending_order: ["Rs. 10,000 ", "PS 5"],
-    }
-
+   
 ]
 
 
