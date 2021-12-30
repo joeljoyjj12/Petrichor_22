f29576c027b6961a3705e3526ca1692aed46727b
Jerry John Thomas <jerryjohnthomastvm@gmail.com>
2021-12-31 02:54:09 +0530

    did 5 of 20 cult events

--- src/data/Cultural_Events_data.js
+++ src/data/Cultural_Events_data.js
@@ -7,7 +7,7 @@ const Cult_events=[
         // date to compete link
         registeration_link : "https://www.google.com/",
         // the pic should be locally uploaded
-        picture: "/media/Event_Cultural/dance1.jpg",
+        picture: "/media/Event_Cultural/soliloquy.jpg",
         organiser:[
             {
                 name : "Harsh Kanani",
@@ -69,7 +69,7 @@ const Cult_events=[
         // date to compete link
         registeration_link : "https://www.google.com/",
         // the pic should be locally uploaded
-        picture: "/media/Event_Cultural/dance1.jpg",
+        picture: "/media/Event_Cultural/Relive_it.jpg",
         organiser:[
             {
                 name : "Harsh Kanani",
@@ -122,7 +122,7 @@ const Cult_events=[
         prizes_in_ascending_order: ["Rs. 3,000 ", "Rs. 1500","Rs. 750"],
     },
     {
-        name : "BGMI",
+        name : "Valorant",
         // jerry check if it is yyyyy-mm-dd or reverse
         start_date : "12-02-2021",
         end_date : "14-02-2022",
@@ -168,7 +168,7 @@ const Cult_events=[
                 "1 kill = 1 point", 
             ],
             ["Position and Points",
-                "15 points",
+            "15 points",
             "12 points",
             "10 points",
             "8 points",
@@ -183,9 +183,150 @@ const Cult_events=[
         judging_criteria_show : false,
 
         prizes_general_text : "We have exciting prizes awaiting you",
-        prizes_in_ascending_order: ["Rs. 2,000 ", "Rs. 1750","Rs. 1250"],
+        prizes_in_ascending_order: ["Rs. 2,000 ", "Rs. 1500","Rs. 1000"],
     },
-   
+    {
+        name : "BGMI",
+        // jerry check if it is yyyyy-mm-dd or reverse
+        start_date : "12-02-2021",
+        end_date : "14-02-2022",
+        // date to compete link
+        registeration_link : "https://www.google.com/",
+        // the pic should be locally uploaded
+        picture: "/media/Event_Cultural/BGMI.jpg",
+        organiser:[
+            {
+                name : "Kaustubh",
+                pic :"/media/organisers/Kaustubh.jpg",
+                email :"7875384916",
+            },
+            {
+                name : "Syed Abdul Manteen",
+                pic :"/media/organisers/Syed_Abdul_Manteen.jpg",
+                email :"6303868362",
+            },
+        ],
+        // first element will be bolded, I think u can only have 2 paras (map)
+        description: ["StudioSeven is Maltas leading A/V and production hub. In 2019, StudioSeven celebrated their 40 year anniversary and contacted Bison Studio in order to help them create an online presence that was in line with their new vision. A website that truly represents what they’re all about", 
+        "From the start of this project, we knew that StudioSeven wanted to stand out. They were not looking for a template approach that would look the same as their competitors. We instantly knew that this was the perfect project for us."
+        ],
+        hover_description : "E Games",
+       
+        // have each rule as a element of an array
+        rule_book_rules :["Players registering for the tournament must have completed minimum age of 16 years. If a player is over the age of 16 but under 18, he/she needs to provide a consent form from their parents or guardians.",
+        "Ranking Requirements: All players on a Team’s roster must have held a ranking of level 15 in the game and above Platinum V Tier, at the time of registration.",
+        "Single Team Exclusivity: A Team Member or the captain is allowed to compete for the one Team that they have an agreement with. A Team Member or coach will not be allowed to compete for more than one team.",
+        "Each Team must maintain, during the tournament exactly four players which is the minimum and maximum number of players allowed.",
+        "Only the registered players can play in the tournament. (If any other player is caught playing, then the team will be disqualified.)",
+        "Each team must select their captain who shall be responsible for communications with the officials of the tournament. A team cannot change the team captain during the tournament without prior approval from the tournament officials.",
+        "Players must not use an emulator to play on a PC or other device that is not a handheld device."],
+
+        //recommended always show true,
+        //instructure in each array row, the first 
+        structure_show:true,
+        structure:[
+            [
+                "Match structure",
+                "Game mode: Battle Royale (25 teams)", 
+                "There will be 3 matches on (Erangle, Miramar, Sanhok)",
+                "1 kill = 1 point", 
+            ],
+            ["Position and Points",
+            "15 points",
+            "12 points",
+            "10 points",
+            "8 points",
+            "6 points",
+            "4 points",
+            "2 points",
+            "1 points",
+            ]
+            ],
+        
+        // reccommended make is true and write down the rest
+        judging_criteria_show : false,
+
+        prizes_general_text : "We have exciting prizes awaiting you",
+        prizes_in_ascending_order: ["Rs. 2,000 ", "Rs. 1500","Rs. 1000"],
+    },
+ 
+    
+    {
+        name : "Call of Duty Mobile",
+        // jerry check if it is yyyyy-mm-dd or reverse
+        start_date : "12-02-2021",
+        end_date : "14-02-2022",
+        // date to compete link
+        registeration_link : "https://www.google.com/",
+        // the pic should be locally uploaded
+        picture: "/media/Event_Cultural/Call_of_Duty_Mobile.jpg",
+        organiser:[
+            {
+                name : "Amar Hamdan",
+                pic :"/media/organisers/Amar_Hamdan.jpg",
+                email :"8606665885",
+            },
+            {
+                name : "Syed Abdul Manteen",
+                pic :"/media/organisers/Syed_Abdul_Manteen.jpg",
+                email :"6303868362",
+            },
+        ],
+        // first element will be bolded, I think u can only have 2 paras (map)
+        description: ["StudioSeven is Maltas leading A/V and production hub. In 2019, StudioSeven celebrated their 40 year anniversary and contacted Bison Studio in order to help them create an online presence that was in line with their new vision. A website that truly represents what they’re all about", 
+        "From the start of this project, we knew that StudioSeven wanted to stand out. They were not looking for a template approach that would look the same as their competitors. We instantly knew that this was the perfect project for us."
+        ],
+        hover_description : "E Games",
+       
+        // have each rule as a element of an array
+        rule_book_rules :["Hackers, Emulators and external controllers are strictly not allowed. If revealed at any point, the entire team will be disqualified. ",
+        "All participants must be 17 years old or above. ",
+        "Participants must use polite language during the gameplay and within the team. Any forms of harassment including verbal, non-verbal, profanity or cursing are not allowed. Be respectful to everyone. ",
+        "Defying any of the above rules will result in the disqualification of the entire team. The decisions of the organizers are final. ",
+        "Further instructions, game schedule etc.. will be provided after registration. So participants must keep a regular check of their mail and the added event group. Any sort of miscommunication or delayed response will not be entertained."],
+
+        //recommended always show true,
+        //instructure in each array row, the first 
+        structure_show:true,
+        structure:[
+            [
+                "Registration",
+                "Participants have to fill the google form to be eligible for the competition before the deadline.. Note: Only the team leader should fill the form on behalf of all the members and only one form is accepted per team ",
+                "Participants can make a team of maximum 4 players. ",
+                "In the form, the leader should fill the following details of each player Full Name, Email id, Whatsapp number, Call of duty player name, School/College name and other general details. ",
+                "CODM player-id must be mentioned correctly with proper capital/small letters, special characters, symbols, numbers etc... ",
+                "Further corrections in this data will not be accepted. ",
+                "If players are not able to form teams with 4 players, they will be paired with other participants in the competition by the organizers",
+            ],
+                 
+            [
+            "Gameplay",
+            "Participants must ensure that their game is updated to the latest version.",
+            "Participants must make sure that they have downloaded all the maps of multiplayer and battle royale.",
+            "It is recommended that you be above level 7 and at least Elite-1 rank. Otherwise you may find it difficult to team up with other players",
+            "Round-1 will be a Squad battle royale match. All the standard rules of battle royale match apply. You will be added to a private match lobby with your team. Depending on the number of teams, more than one lobby may be created. In such a case, another round will be conducted with the top teams from each round. Top 5 teams from this round will qualify to round 2.",
+            "In Round-2, the chosen 5 teams will compete with each other in Multiplayer-Domination Matches. The final leaderboard from this round decides the winners.",
+            "In case of a tie at any point, Multiplayer-Team deathmatch will be used as a tie breaker.",
+            "All the class abilities, score streaks and operator skills are allowed. Hence, it is recommended that you unlock them all before the competition.",
+            ],    
+            
+            [
+            "General", 
+            "Participants have to join a discord/whatsapp group made by the organizers. Link to the groups will be shared via email after the registration. All further discussions will only be made in the group.",
+            "Participants must ensure proper network connection, microphone and power supply. Any request for re-conducting the game will not be entertained due to these issues.",
+            "Participants must be online and have to join the private room at least 10 minutes before the mentioned time of the game. Otherwise the match will start without him/her with less members in that team.",
+            "Certificates will only be issued to those who participate in all the rounds of the game.",
+            "In case there is any change in the team member, it must be mentioned before commencement of Round-1. Otherwise, he/she will be removed from the lobby",
+            ],
+            ],
+        
+        // reccommended make is true and write down the rest
+        judging_criteria_show : false,
+
+        prizes_general_text : "We have exciting prizes awaiting you",
+        prizes_in_ascending_order: ["Rs. 2,000 ", "Rs. 1500","Rs. 1000"],
+    },
+
 ]
 
 
