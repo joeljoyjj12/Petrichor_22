53d53c35a99965416fcc288b691702617eb3e99f
Joel Joy <121901018@smail.iitpkd.ac.in>
2022-01-03 01:46:39 +0530

    Home event card filter

--- src/Home/Jerry_Home2/Cult_tech.js
+++ src/Home/Jerry_Home2/Cult_tech.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Cult_tech.css'
 import { Link } from 'react-router-dom'
+import guitar from "./Images/guitar.jpg"
+import technical1 from "./Images/technical1.jpg"
 
 function Cult_tech() {
     return (
@@ -9,18 +11,20 @@ function Cult_tech() {
                 <h1 id="home_heading_contact" className=' event_head_home'><span style={{color:"rgb(201 23 23)"}}>E</span>vents</h1>
                 <div className="row justify-content-around wrap_cult_tech">
                     
-                    <div className="col-lg-5 col-md-6 col-sm-6 col-11 home_tech_1" data-aos='flip-right'>
+                    <div className="col-lg-5 col-md-10 col-sm-10 col-11 home_evnt home_tech_1" data-aos='flip-right' data-aos-duration="900">
                         <Link style={{textDecoration:"none"}} to="/technical_events">
                             <div className="event_descr_home row justify-content-center align-items-center">
-                                <div>Technical</div>
+                                <img src={technical1} className="jerry_ct_pic jerry_ct_pic_s2" alt="" />
+                                <h1 className="home_event_desc">Technical</h1>
                             </div>
                         </Link>
                     </div>
 
-                    <div className="col-lg-5 col-md-6 col-sm-6 col-11 home_cult_1" data-aos="flip-left">
+                    <div className="col-lg-5 col-md-8 col-sm-9 col-11 home_evnt home_cult_1" data-aos="flip-left" data-aos-duration="900">
                         <Link style={{textDecoration:"none"}} to="/cultural_events">
                             <div className="event_descr_home row justify-content-center align-items-center">
-                                    <div>Cultural</div>
+                                <img src={guitar} className="jerry_ct_pic jerry_ct_pic_s1" alt="" />
+                                <h1 className="home_event_desc">Cultural</h1>
                             </div>
                         </Link>
                     </div>
--- src/Workshops/Workshops.js
+++ src/Workshops/Workshops.js
@@ -1,9 +1,71 @@
-import React from 'react'
+import React,{useEffect} from 'react'
+import './Workshops.css'
+import Card from './Card.js'
 
-function Workshops() {
+import img1 from "./Images/ic_engine.jpg"
+import img2 from "./Images/robot.jpg"
+
+import Aos from "aos";
+import "aos/dist/aos.css"
+
+
+function Workshops(){
+    useEffect(()=>{
+        Aos.init({duration: 700});
+    },[])
+
     return (
-        <div>
-            workshop
+        <div style={{paddingTop:"15vh"}}>
+            <div className="row justify-content-center workshop_card">
+                <div className="col-7 col-lg-7 col-md-8 col-sm-9 col-10" data-aos="flip-up" data-aos-delay="300">
+                    <Card 
+                    title="energy to mobility : Basics of ic engines"
+                    desc="Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power, without direct active management by the user."
+                    color="#B2F296"
+                    deadline="15/01/2022"
+                    date="JANUARY 19-27"
+                    speaker="Dr. Anand TNC"
+                    price="300"
+                    bg={img1}
+                    />
+                </div>
+                <div className="col-7 col-lg-7 col-md-8 col-sm-9 col-10" data-aos="flip-down">
+                    <Card 
+                    title="wheeled robot modeling and simulation in python"
+                    desc="Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power, without direct active management by the user."
+                    color="#FFE800"
+                    deadline="15/01/2022"
+                    date="JANUARY 19-27"
+                    speaker="Dr. Anand TNC"
+                    price="300"
+                    bg={img2}
+                    />
+                </div>
+                <div className="col-7 col-lg-7 col-md-8 col-sm-9 col-10" data-aos="flip-up">
+                    <Card 
+                    title="Basics of Modelling using Fusion 360"
+                    desc="Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power, without direct active management by the user."
+                    color="#6D92DC"
+                    deadline="15/01/2022"
+                    date="JANUARY 19-27"
+                    speaker="AUTODESK"
+                    price="400"
+                    bg={img1}
+                    />
+                </div>
+                <div className="col-7 col-lg-7 col-md-8 col-sm-9 col-10" data-aos="flip-down">
+                    <Card 
+                    title="Machine Learning "
+                    desc="Cloud computing is the on-demand availability of computer system resources, especially data storage and computing power, without direct active management by the user."
+                    color="#E7A012"
+                    deadline="15/01/2022"
+                    date="JANUARY 19-27"
+                    speaker="Dr. Anand TNC"
+                    price="700"
+                    bg={img2}
+                    />
+                </div>
+            </div>
         </div>
     )
 }
