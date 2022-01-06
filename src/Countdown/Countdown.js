bbd2797b37cef7c395fe929f0ce42049c5c9a42b
Jerry John Thomas <jerryjohnthomastvm@gmail.com>
2022-01-06 14:58:30 +0530

    added  2 videos to countdown

--- src/Countdown/Countdown.js
+++ src/Countdown/Countdown.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React ,{useState} from 'react'
 import Timer from "./Timer.js"
 import promo from "./promo.mp4"
+import coming_soon_mobile from "./assets/coming_soon_mobile.mp4"
+import coming_soon_website from "./assets/coming_soon_website.mp4"
+
 function Countdown() {
+    let [isphone,setIsphone]=useState(false);
     React.useEffect(()=>{
         let aa=document.querySelector(".progress");
         aa.style.display="none";
+
+        // let w = window.screen.width;
+        let  w = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0)
+        console.log(w);
+        if(w>650)
+            setIsphone(false);
+        else    
+            setIsphone(true);
     },[])
 
     return (
@@ -12,9 +24,21 @@ function Countdown() {
             
 
             <div className="cd_new_cd">
-< video class="cd_video" style={{width:"100vw",height:"100vh"}} width="1500px" height="100%" muted Autoplay="autoplay" playsinline  loop>
-                <source src={promo} type="video/webm"/>
+           {isphone?
+             < video class="mobile_promo_video" 
+                // style={{width:"100vw",height:"100vh"}} width="1500px" height="100%" 
+             muted Autoplay="autoplay" playsinline  loop>
+                <source src={coming_soon_mobile} type="video/webm"/>
             </video>
+           :
+           < video class="lap_promo_video" 
+                //    style={{width:"100vw",height:"100vh"}} width="1500px" height="100%" 
+           muted Autoplay="autoplay" 
+            playsinline  
+           loop>
+                <source src={coming_soon_website} type="video/webm"/>
+            </video>
+           }
             </div>
 
             <div className="ayush_countdown_App jerry_cd_full" >
